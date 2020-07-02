c50b8a667fe4d2519b13e2f0c4957230a3c3d4ef
Дмитрий Гриневич <d2.grinevich@gmail.com>
2020-07-02 17:57:51 +0300

    lecture_11

---
+++ Square/Square/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Transactions;
+
+class Program
+{
+
+    static double CountСircle(double text)
+
+    {
+        return text * text * Math.PI;
+    }
+    static double CountSquare(double text1)
+    {
+        return text1 * text1;
+    }
+    static double CountTriangle(double text2, double text3)
+    {
+        return (text2 * text3) / 2;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ввести радиус круга для вычисления площади");
+        double text = Convert.ToDouble(Console.ReadLine());
+        double resultCircle = CountСircle(text);
+        Console.WriteLine("Площадь круга = " + resultCircle);
+        Console.WriteLine("Введите сторону квадрата для вычисления площади");
+        double text1 = Convert.ToDouble(Console.ReadLine());
+        double resultSquare = CountSquare(text1);
+        Console.WriteLine(resultSquare);
+        Console.WriteLine("Введите основание треугольника: \n Введите высоту треугольника: ");
+        double text2 = Convert.ToDouble(Console.ReadLine());
+        double text3 = Convert.ToDouble(Console.ReadLine());
+        double resultTriangle = CountTriangle(text2, text3);
+        Console.WriteLine("Площадь треугольника = " + resultTriangle);
+
+
+    }
+}
